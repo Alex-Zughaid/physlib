8ecdf7d4720e27d0866e2e48d143631061378542
Alex-Zughaid <117576511+Alex-Zughaid@users.noreply.github.com>
2026-09-07 21:59:53 +0100

    small fix

--- Physlib/Mathematics/OneParameterSubgroups/Unitary.lean
+++ Physlib/Mathematics/OneParameterSubgroups/Unitary.lean
@@ -32,7 +32,7 @@ Physlib.
 ## References
 
 * M. H. Stone, *Linear Transformations in Hilbert Space III. Operational Methods and Group Theory*,
-  Proc. Natl. Acad. Sci. 18 (1932), 172-175. [ref: stone_1932]
+  Proc. Natl. Acad. Sci. 16 (1930), 172-175. [ref: stone_1930]
 -/
 
 @[expose] public section
