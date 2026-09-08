637a954468ba6ef8dabdd2b6d18729d537ec9bbe
Alex Zughaid <117576511+Alex-Zughaid@users.noreply.github.com>
2026-09-08 10:36:24 +0100

    Update Physlib/StringTheory/FTheory/SU5/Quanta/IsViable.lean
    Co-authored-by: Daniel Morrison <39346894+morrison-daniel@users.noreply.github.com>

--- Physlib/StringTheory/FTheory/SU5/Quanta/IsViable.lean
+++ Physlib/StringTheory/FTheory/SU5/Quanta/IsViable.lean
@@ -54,7 +54,7 @@ lake exe graph --from
 
 ## iv. References
 
-* The key reference for the material in this module. [ref: arxiv_1507_05961]
+* Froggatt-Nielsen meets Mordell-Weil: A Phenomenological Survey of Global F-theory GUTs with U(1)s (arxiv:1507.05961). [ref: arxiv_1507_05961]
 -/
 
 @[expose] public section
